3832b327ea097cf65c04ee57a29f06d189bab8cf
Aayush2302 <ayushjivani2201@gmail.com>
2025-12-30 01:36:09 +0530

    allowed google headers

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -24,7 +24,7 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-google-id-token"],
   })
 );
 
